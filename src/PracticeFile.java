1d3239cd2be9e8d99f80a3e03e72ccf7a45e6f78
Yeva22 <y.shimanayev@gmail.com>
2022-10-18 20:05:02 -0400

    New trial code.

---
+++ src/PracticeFile.java
@@ -0,0 +1,12 @@
+package src;
+
+public class PracticeFile {
+    public static void main(String[] args) {
+        int j = 88;
+        int y = 99;
+
+        System.out.println(j+y);
+        System.out.println("this is the sum of " +j + " and" + y);
+
+    }
+}
